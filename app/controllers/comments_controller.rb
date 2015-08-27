814d529b5ee954227c51ebf983870ae33def5388
alberto <hello@albertomar.in>
2015-08-27 12:15:50 +0200

    Add authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+	http_basic_authenticate_with name: "user", password: "password",
+	only: :destroy
+
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
@@ -7,7 +10,7 @@ class CommentsController < ApplicationController
 
 	def destroy
 		@article = Article.find(params[:article_id])
-		@comment = @article.comments.find(comment_params)
+		@comment = @article.comments.find(params[:id])
 		@comment.destroy
 		redirect_to article_path(@article)
 	end
